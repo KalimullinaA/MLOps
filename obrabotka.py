ae99669bdd8e962e2455d79ca76215c565bab9e7
Elvina8 <Kalimullina.elvina2016@yandex.ru>
2023-03-08 18:59:02 +0500

    processing adjustment

--- obrabotka.py
+++ obrabotka.py
@@ -2,27 +2,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-plt.style.use('ddplot')
-plt.rcParams ['figure.figsize'] = (10,5)
+#загрузка данных
+
+DF_train = pd.read_csv('', delimiter =',', index_col = 0)
+DF_test = pd.read.csv('',delimiter =',', index_col = 0)
+Target = pd.read_csv('',delimiter =',', index_col = 0)
+
+#удаление дубликатов, объединение DF
+
+DF_train.drop_duplicates(inplace = True)
+
+DF_total = pd.concat([DF_train, DF_test])
+DF_films = pd.concat([Target, Submission])
+DF_total = DF_total.join(DF_films)
+
+#Анализ данных
+
+DF_total.info() #просмотр данных
+
+# Разделение столбцов на категориальные и числовые
+
+сateg_columns = []
+num_columns = []
+
+for column_name in DF_total.columns:
+    if (DF_total[columns_name].dtypes == object):
+        categ_columns +=[column_name]
+    else:
+        num_columns +=[column_name]
+
+print ('categorical columns:\t',categ_columns, '\n len = ', len(categ_columns))
+print ('numerical columns:\t',num_columns, '\n len = ', len(num_columns))
 
 
 
 
 
-# считываем из csv файла подготовленный датасет (Лена) для обучения
-
-data_train = pd.read_csv('data_train.csv')
-films [['name', 'rating', 'genre', 'run_time', 'tagline']] [:20]
-#films ['raiting'].values_counts() выбор по рейтингу, написать,чтобы выводился фильмец
-#films_counts = films['raiting'].value_counts()
-#films_counts[:15]
-#построение графика самых ужасных фильмов по типу аватар, ну или самых лучших
-film_counts[:15].plot(kind = 'bar')
-X_train = data_train[['']].values
-y_train = data_train[''].values
-
-#загружаем модель МО
-from sklearn.linear_model import LogisticRegression
-model = LogisticRegresiion(max_iter=100_000).fit(X_train,y_train)
-
-#сохранение модели
