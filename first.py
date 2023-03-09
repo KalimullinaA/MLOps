d03c9ddb76d9bc5fb718918830d24d919661fbb3
KukartsevaElena <kukarceva_elena@mail.ru>
2023-03-09 20:27:59 +0500

    data creation

---
+++ first.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+dataframe = pd.read_csv(r"C:\Users\Admin\Documents\GitHub\MLOps\data\IMDB Top 250 Movies.csv")
+print(dataframe)
+
+train, test = train_test_split(dataframe, test_size=0.2)
+# train, val = train_test_split(train, test_size=0.2)
+print(len(train), 'train examples')
+# print(len(val), 'validation examples')
+print(len(test), 'test examples')
